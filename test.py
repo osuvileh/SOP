1ad7a1441fda13a02bb15b5be3e14c7b543f002a
jejatu <jejatu@gmail.com>
2015-03-16 12:19:08 +0200

    Fixed labeling, added names to objects, add object to database if object is not matched, added new test video test2.mp4

--- test.py
+++ test.py
@@ -5,45 +5,49 @@ from numpy import reshape, uint8, flipud
 from scipy import ndimage
 from scipy.cluster.vq import kmeans, vq
 from skimage.morphology import disk, diamond
+from skimage.measure import label
+from skimage.morphology import closing, square
+from skimage.segmentation import clear_border
 
 
 class Object:
 	featureVector = None
 	
 class Feature:
-	def __init__(self, keypoints, descriptors):
+	def __init__(self, keypoints, descriptors, name):
 		self.keypoints = keypoints
 		self.descriptors = descriptors
+		self.name = name
 		
 class Match:
 	def __init__(self, dbFeature, feature, keypointPairs):
 		self.dbFeature = dbFeature
 		self.feature = feature
 		self.keypointPairs = keypointPairs
+		self._calculateBoundingBox()
+		
+	def _calculateBoundingBox(self):
+		minX = 9999
+		minY = 9999
+		maxX = -9999
+		maxY = -9999
+		for pair in self.keypointPairs:
+			if minX > pair[1].pt[0]:
+				minX = pair[1].pt[0]
+			if minY > pair[1].pt[1]:
+				minY = pair[1].pt[1]
+			
+			if maxX < pair[1].pt[0]:
+				maxX = pair[1].pt[0]
+			if maxY < pair[1].pt[1]:
+				maxY = pair[1].pt[1]
+				
+		self.min = (int(minX), int(minY))
+		self.max = (int(maxX), int(maxY))
 	
 class Database:
 	def __init(self):
 		pass
-
-def reduceVal(val):
-	#used for colour space quantization to 27 colours
-	if val < 64:
-		return 0
-	if val < 128:
-		return 64
-	return 255
-	
-def colourQuantization(image):
-	red = image[:,:,2]
-	green = image[:,:,1]
-	blue = image[:,:,0]
-
-	for j in range(480):
-		for i in range(640):
-			image[:,:,2][i,j] = reduceVal(red[i,j])
-			image[:,:,1][i,j] = reduceVal(green[i,j])
-			image[:,:,0][i,j] = reduceVal(blue[i,j])
-	return image
 	
 def segmentation(image):
 	"""Executes image segmentation based on various features of the video stream"""
@@ -53,25 +57,29 @@ def segmentation(image):
 	ratio = 3
 	kernel_size = 3
 	gray = cv2.cvtColor(image, cv2.cv.CV_RGB2GRAY)
-	blurred = cv2.GaussianBlur(gray,(5,5),0)
+	blurred = cv2.GaussianBlur(gray, (5, 5), 0)
 
-	ret, bw = cv2.threshold(blurred, 0,255, cv2.THRESH_BINARY+cv2.THRESH_OTSU)
+	ret, bw = cv2.threshold(blurred, 0, 255, cv2.THRESH_BINARY + cv2.THRESH_OTSU)
 	#bw = cv2.adaptiveThreshold(blurred, 255, cv2.ADAPTIVE_THRESH_MEAN_C, cv2.THRESH_BINARY,11,0)
-	dilation = cv2.dilate(bw,disk(4),iterations=1)
-	erosion = cv2.erode(dilation,disk(4),iterations = 1)
+	
+	result = cv2.dilate(bw, square(10), iterations = 1)
+	result = cv2.erode(result, square(10), iterations = 1)
+	
+	result = cv2.erode(result, square(10), iterations = 1)
+	result = cv2.dilate(result, square(10), iterations = 1)
 	
 	
 	#quantized = equalize(image, diamond(5))
-	edges = cv2.Canny(image, lowThreshold, max_lowThreshold)
+	#edges = cv2.Canny(image, lowThreshold, max_lowThreshold)
 
-	dilateEdges = cv2.dilate(edges,disk(4),iterations=1)
-	erodeEdges = cv2.erode(dilateEdges,disk(3), iterations=1)
-	area = erosion | erodeEdges
+	#dilateEdges = cv2.dilate(edges,disk(4),iterations=1)
+	#erodeEdges = cv2.erode(dilateEdges,disk(3), iterations=1)
+	#area = erosion | erodeEdges
 	#area = cv2.erode(area,disk(3), iterations=1)
 	#area = cv2.dilate(area, disk(3), iterations=1)
-	area, labels = ndimage.label(area)
-	area = area*50
-	return area
+	#area, labels = ndimage.label(area)
+	#area = area*50
+	return label(result) * 50
 	
 def extractSegments(image, segmented):
 	segments = []
@@ -83,13 +91,14 @@ def extractSegments(image, segmented):
 		segments.append(segment)
 	return segments;
 
-def featureExtractor(detector, extractor, segments):
+def featureExtractor(detector, extractor, segments, frameNumber):
 	"""Extracts features from segmented image(s)"""
 	features = []
-	for segment in segments:
-		keypoints = detector.detect(segment)
-		keypoints, descriptors = extractor.compute(segment, keypoints)
-		features.append(Feature(keypoints, descriptors))
+	for i, segment in enumerate(segments):
+		ret, mask = cv2.threshold(segment, 0, 255, cv2.THRESH_BINARY)
+		keypoints = detector.detect(segment, mask)
+		keypoints, descriptors = extractor.compute(segment, keypoints, mask)
+		features.append(Feature(keypoints, descriptors, str(frameNumber) + str(i)))
 	return features;
 
 def matchFinder(features):
@@ -100,7 +109,7 @@ def addToDatabase(object):
 	#
 	pass
 	
-def filterMatches(kp1, kp2, matches, ratio = 0.75):
+def filterMatches(kp1, kp2, matches, ratio = 0.6):
     mkp1, mkp2 = [], []
     for m in matches:
         if len(m) == 2 and m[0].distance < m[1].distance * ratio:
@@ -127,31 +136,28 @@ def main():
 		print "saving labeled segmentation"
 		cv2.imwrite("%i%s" % (frameNumber, 'labels.jpg'), segmented)
 		segments = extractSegments(frame, segmented)
-		features = featureExtractor(detector, extractor, segments)
+		features = featureExtractor(detector, extractor, segments, frameNumber)
 		
 		featureMatches = []
-		for a, data in enumerate(database):
-			for b, feature in enumerate(features):
+		for a, feature in enumerate(features):
+			isMatch = False
+			for b, data in enumerate(database):
 				if (data.descriptors != None and feature.descriptors != None):
-					matches = matcher.knnMatch(data.descriptors, trainDescriptors = feature.descriptors, k = 2)
+					matches = matcher.knnMatch(data.descriptors, feature.descriptors, k = 2)
 					pairs = filterMatches(data.keypoints, feature.keypoints, matches)
 					if len(pairs) >= 7:
 						featureMatches.append(Match(data, feature, pairs))
-		
-		#index = 0
-		#frame[segments[index] == 0] = 0 
-		#for keypoint in features[index].keypoints:
-		#	cv2.circle(frame, (int(keypoint.pt[0]), int(keypoint.pt[1])), 4, (255, 0, 0), thickness=1, lineType=8, shift=0)
+						isMatch = True
+			if isMatch == False:
+				database.append(feature)
 		
 		colorIndex = 0
 		for match in featureMatches:
+			cv2.rectangle(frame, match.min, match.max, colors[colorIndex % len(colors)], 2)
+			cv2.putText(frame, match.dbFeature.name, match.min, cv2.FONT_HERSHEY_PLAIN, 2, colors[colorIndex % len(colors)], 2)
 			for pair in match.keypointPairs:
-				cv2.line(frame, (int(pair[0].pt[0]), int(pair[0].pt[1])),(int(pair[1].pt[0]), int(pair[1].pt[1])),colors[colorIndex % len(colors)],1)
+				cv2.line(frame, (int(pair[0].pt[0]), int(pair[0].pt[1])),(int(pair[1].pt[0]), int(pair[1].pt[1])), colors[colorIndex % len(colors)], 1)
 			colorIndex += 1
-				
-		database = []
-		for feature in features:
-			database.append(feature)
 		
 		cv2.imwrite("%i%s" % (frameNumber, '.jpg'), frame)
 		print 'saving image', frameNumber
